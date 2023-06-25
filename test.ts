05ab51ff490f7606c22c93fa39d5b928f6a7362d
Ivan Kraus <91247726+Kraus-Ivan@users.noreply.github.com>
2023-06-25 22:53:03 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,31 +1,13 @@
-let servo1 = servoPWM.createServo(AnalogPin.P13);
-let servo2 = servoPWM.createServo(AnalogPin.P15);
-
-// změřeno
-servo1.setMinPulse(400);
-servo1.setMaxPulse(2600);
-
-servo2.setMinPulse(400);
-servo2.setMaxPulse(2600);
-
-
-const step = 1;
-let currentPosition = 2000;
-servo1.setPulse(currentPosition);
-servo2.setPulse(currentPosition);
-
 basic.forever(function () {
-    basic.pause(2);
+    basic.pause(50);
+    smoothServo.SmoothServo.update();
+});
 
-    if (input.buttonIsPressed(Button.A)){
-        currentPosition += step; // Zvyšuje hodnotu pulzu o krok
-        servo1.setPulse(currentPosition);
-        servo2.setPulse(currentPosition + 150)
-    }
+let first_servo = smoothServo.SmoothServo.createServo(AnalogPin.P0);
+let second_servo = smoothServo.SmoothServo.createServo(AnalogPin.P1);
 
-    if (input.buttonIsPressed(Button.B)) {
-        currentPosition -= step; // Sníží hodnotu pulzu o krok
-        servo1.setPulse(currentPosition);
-        servo2.setPulse(currentPosition - 150);
-    }
+input.onButtonPressed(Button.A, () => {
+    first_servo.moveTo(2000);
+    console.log(first_servo.buffer());
+    console.log(first_servo.toString());
 });
